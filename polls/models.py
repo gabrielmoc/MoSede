88b084358f1671ed75fd5059617a0f65ba2c58cf
Antonio Valença <antoniotvalenca@gmail.com>
2023-04-15 19:11:29 -0300

    .

---
+++ polls/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.product_nome
+        
+class Buy(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    amount = models.PositiveIntegerField(default=1)
+    total = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user} comprou {self.amount} x {self.product} em {self.date}"
